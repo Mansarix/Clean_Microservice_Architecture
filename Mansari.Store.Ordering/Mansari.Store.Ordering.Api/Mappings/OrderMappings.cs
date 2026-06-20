f2fe13742445ca956d14e6d933638887da9e146a
Mahmood Ansari <60201069+Mansarix@users.noreply.github.com>
2026-06-20 09:46:54 +0330

    add ordering microservice

---
+++ Mansari.Store.Ordering/Mansari.Store.Ordering.Api/Mappings/OrderMappings.cs
@@ -0,0 +1,13 @@
+﻿using Mansari.Store.Ordering.Api.Contracts;
+using Mansari.Store.Ordering.Application.Orders.Commands.CreateOrder;
+
+namespace Mansari.Store.Ordering.Api.Mappings;
+
+public static class OrderMappings
+{
+    public static CreateOrderCommand ToCommand(this CreateOrderRequest request)
+        => new CreateOrderCommand(request.BookId, request.Quantity);
+
+    public static CreateOrderResponse ToResponse(this CreateOrderResponse)
+        => new();
+}
